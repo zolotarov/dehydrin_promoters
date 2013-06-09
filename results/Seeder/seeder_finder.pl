a1e3d7ebdafed9f7f069014243bc03d50ef0f6e8
Yevgen Zolotarov <yevgen.zolotarov@mail.mcgill.ca>
2013-06-09 19:12:46 -0400

    all dehydrins seeder analyssi

--- results/Seeder/seeder_finder.pl
+++ results/Seeder/seeder_finder.pl
@@ -5,8 +5,8 @@ use Seeder::Finder;
     motif_width   => "12", 
     n_motif       => "10", 
     hd_index_file => "../../../Seeder/6.index", 
-    seq_file      => "/home/yzolotarov/dehydrin_promoters/coexpression/arath_KSdeh_NASCArrays.fas", 
-    bkgd_file     => "arath.bkgd", 
-    out_file      => "arath_KSdeh_NASCArrays.finder", 
+    seq_file      => "/home/yzolotarov/dehydrin_promoters/sequences/promoters/all_promoters.fas", 
+    bkgd_file     => "used_plants.bkgd", 
+    out_file      => "all_dehydrins.finder", 
 ); 
 $finder->find_motifs;
