5cc0c5cfffb1f62728e341cdee43ed18dbd47d17
Yevgen Zolotarov <yevgen.zolotarov@mail.mcgill.ca>
2013-06-11 14:06:45 -0400

    added KS with Oxytropis results

--- results/Seeder/seeder_finder.pl
+++ results/Seeder/seeder_finder.pl
@@ -5,8 +5,8 @@ use Seeder::Finder;
     motif_width   => "12", 
     n_motif       => "10", 
     hd_index_file => "../../../Seeder/6.index", 
-    seq_file      => "/home/yzolotarov/dehydrin_promoters/sequences/promoters/KS_dehydrins_promoters_oneline_no_474716.fas", 
+    seq_file      => "/home/yzolotarov/dehydrin_promoters/sequences/promoters/KS_with_oxytropis.fas", 
     bkgd_file     => "used_plants.bkgd", 
-    out_file      => "KS_dehydrins_no_474716.finder", 
+    out_file      => "KS_with_oxytropis.finder", 
 ); 
 $finder->find_motifs;
