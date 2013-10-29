3e5d75f54220ae1a11eca9bd4dabebf97f664b15
Yevgen Zolotarov <yevgen.zolotarov@mail.mcgill.ca>
2013-10-29 13:44:01 -0400

    added WebLogo figures

--- results/Seeder/seeder_finder.pl
+++ results/Seeder/seeder_finder.pl
@@ -5,8 +5,8 @@ use Seeder::Finder;
     motif_width   => "12", 
     n_motif       => "10", 
     hd_index_file => "../../../Seeder/6.index", 
-    seq_file      => "/home/yzolotarov/dehydrin_promoters/sequences/promoters/KS_20130910.fas", 
+    seq_file      => "/home/yzolotarov/dehydrin_promoters/sequences/promoters/basic_pI_promoters.fas", 
     bkgd_file     => "used_plants.bkgd", 
-    out_file      => "KS_20130910.finder", 
+    out_file      => "basic_pI_promoters.finder", 
 ); 
 $finder->find_motifs;
